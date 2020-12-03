2f3b6144be63008ebd292f0cdf95ff05b41843cd
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-12-03 17:05:32 +0000

    abs

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ import {SocLinks} from './components/SocLinks/SocLinks';
 import {Border} from './components/Border/Border';
 import {Content} from './components/Content';
 import {Cube} from './components/Cube/Cube';
-import React, {useEffect} from 'react';
-import ReactGA from 'react-ga';
-import config from './config';
 
 export const App = () => {
-    useEffect(() => {
-        ReactGA.initialize(config.googleAnalytics);
-    }, []);
     return (
         <div className="App">
             <Border>
--- src/config.js
+++ src/config.js
@@ -1,7 +1,18 @@
+import ReactGA from 'react-ga';
+
 export default {
-    /**
-     * Base URL to api
-     */
     emailJsApiKey: process.env.REACT_APP_EMAIL_JS_KEY,
     googleAnalytics: process.env.REACT_APP_GA_TRAKING,
+};
+
+export const GAInit = (TrackingID) => {
+    ReactGA.initialize(TrackingID);
+};
+
+export const GAEvent = (category, action) => {
+    ReactGA.initialize(process.env.REACT_APP_GA_TRAKING);
+    ReactGA.event({
+        category,
+        action,
+    });
 };
