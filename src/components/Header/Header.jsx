9adfecfd5a98d278e5af8128845287167eb946b4
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-12-26 16:14:50 +0000

    removed / fromimage path added custom domain

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import './header.scss'
 export const Header = () => (
     <header>
-        <img src="/images/logo.jpg" alt="Site's Logo '|M L|'"/>
+        <img src="images/logo.jpg" alt="Site's Logo '|M L|'"/>
     </header>
 );
